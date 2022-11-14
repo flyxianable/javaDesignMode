14c5d587fac2612e06926668f4db3f3eb1f1ddba
liangyanbin <1144274802@qq.com>
2022-11-14 15:38:04 +0800

    注释

--- app/src/main/java/com/lyb/designmode/chain/Approver.java
+++ app/src/main/java/com/lyb/designmode/chain/Approver.java
@@ -6,9 +6,7 @@ package com.lyb.designmode.chain;
 public interface Approver {
 
     /**
-     * 如果返回Boolean.TRUE，表示处理成功，如果返回Boolean.FALSE，
      *
-     * 表示处理失败（请求被拒绝），如果返回null，则交由下一个Handler处理。
      * @param request
      * @return
      */
--- app/src/main/java/com/lyb/designmode/factory/simple_factory/IShape.java
+++ app/src/main/java/com/lyb/designmode/factory/simple_factory/IShape.java
@@ -4,5 +4,7 @@ package com.lyb.designmode.factory.simple_factory;
  * 抽象产品
  */
 public interface IShape {
+
     void draw();
+
 }
